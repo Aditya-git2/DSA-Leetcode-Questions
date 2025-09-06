ae41fa0231c76ce8f2f205939314686099ed24a5
Aditya sharma <codebyaditya143@gmail.com>
2025-09-06 16:57:04 +0530

    Time: 2 ms (77.34%), Space: 7.6 MB (98.44%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,16 +1,16 @@
 // 0  1  1  2  3  5  8  13  21 ...........    fibonacci sequence
 class Solution {
 public:
-    // approach 2 => by dp
-    // Helper function to compute Fibonacci using memoization
-    int fibo(int n, vector<int>& dp) {
-        if (n <= 1) return n; // Base case: fib(0) = 0, fib(1) = 1
-        if (dp[n] != -1) return dp[n]; // If already computed,return stored result
-        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
-    }
+    // approach 3 => by dp [Tabulation method]
     int fib(int n) {
-        vector<int> dp(n + 1, -1);
-        return fibo(n, dp);
+       if(n<=1) return n;
+       int dp[n+1];
+       dp[0] = 0;
+       dp[1] = 1;
+       for(int i = 2; i<= n;i++){
+        dp[i] = dp[i-1]+dp[i-2];
+       }
+       return dp[n];
     }
 };
 /* approach1=> by recursion => as it is short and easy problem => not show T.L.E
@@ -22,6 +22,22 @@ public:
         if (n == 0 || n == 1) return n;
         // fib(n) = fib(n-1) + fib(n-2)
         return fib(n - 1) + fib(n - 2);
+    }
+};
+*/
+/*
+class Solution {
+public:
+    // approach 2 => by dp [recursion + memorization]
+    // Helper function to compute Fibonacci using memoization
+    int fibo(int n, vector<int>& dp) {
+        if (n <= 1) return n; // Base case: fib(0) = 0, fib(1) = 1
+        if (dp[n] != -1) return dp[n]; // If already computed,return stored result
+        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+    }
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        return fibo(n, dp);
     }
 };
 */
