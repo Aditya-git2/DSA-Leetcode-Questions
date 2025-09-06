5232ba7bce2a082b5d2e3d22f662ac5ab51655c7
Aditya sharma <codebyaditya143@gmail.com>
2025-09-06 18:36:07 +0530

    Time: 0 ms (100%), Space: 17.8 MB (30.97%) - LeetHub

---
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int helper(vector<int>& cost,int i,vector<int>&dp){  // i => index of stair
+        if(i==0 || i == 1) return cost[i];
+        if(dp[i] != -1) return dp[i];
+        return dp[i] = cost[i] + min(helper(cost,i-1,dp),helper(cost,i-2,dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        vector<int>dp(n,-1);
+        return min(helper(cost,n-1,dp),helper(cost,n-2,dp));
+    }
+};
+
+// //recursive solution => n ---> 0      T.L.E
+// class Solution {
+// public:
+//     int helper(vector<int>& cost,int i){  // i => index of stair
+//         if(i==0 || i == 1) return cost[i];
+//         return cost[i] + min(helper(cost,i-1),helper(cost,i-2));
+//     }
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n = cost.size();
+//         return min(helper(cost,n-1),helper(cost,n-2));
+//     }
+// };
+
+/*   recursive 0---> n    => Time Limit Exceeded
+class Solution {
+public:
+    int helper(vector<int>& cost,int i, int sum){  // i => index of stair
+        if(i>=cost.size()) return sum;
+        return min(helper(cost,i+1,sum+cost[i]),helper(cost,i+2,sum+cost[i]));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        return min(helper(cost,0,0),helper(cost,1,0));
+    }
+};
+*/
