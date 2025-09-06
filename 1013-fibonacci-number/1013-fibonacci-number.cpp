79340a96cdc29e141189cd89c678f7390435e825
Aditya sharma <codebyaditya143@gmail.com>
2025-09-06 16:13:56 +0530

    Time: 10 ms (46.09%), Space: 7.6 MB (87.13%) - LeetHub

---
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    // 0  1  1  2  3  5  8  13  21 ...........    fibonacci sequence
+    int fib(int n) {
+        if (n == 0 || n == 1) return n;
+        // fib(n) = fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2);
+    }
+};
