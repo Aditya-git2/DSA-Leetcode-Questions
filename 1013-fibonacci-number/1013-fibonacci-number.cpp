8c88ca62cb6c914267962954e94eb02ded1949ad
Aditya sharma <codebyaditya143@gmail.com>
2025-09-06 16:46:31 +0530

    Time: 0 ms (100%), Space: 8.2 MB (11.56%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -1,9 +1,27 @@
+// 0  1  1  2  3  5  8  13  21 ...........    fibonacci sequence
+class Solution {
+public:
+    // approach 2 => by dp
+    // Helper function to compute Fibonacci using memoization
+    int fibo(int n, vector<int>& dp) {
+        if (n <= 1) return n; // Base case: fib(0) = 0, fib(1) = 1
+        if (dp[n] != -1) return dp[n]; // If already computed,return stored result
+        return dp[n] = fibo(n - 1, dp) + fibo(n - 2, dp);
+    }
+    int fib(int n) {
+        vector<int> dp(n + 1, -1);
+        return fibo(n, dp);
+    }
+};
+/* approach1=> by recursion => as it is short and easy problem => not show T.L.E
 class Solution {
 public:
     // 0  1  1  2  3  5  8  13  21 ...........    fibonacci sequence
     int fib(int n) {
+        // if(n<=1) return n;     // n is not -ve
         if (n == 0 || n == 1) return n;
         // fib(n) = fib(n-1) + fib(n-2)
         return fib(n - 1) + fib(n - 2);
     }
 };
+*/
