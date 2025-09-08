7dbb3f9c310281007d791d39f405e1854749f840
Aditya sharma <codebyaditya143@gmail.com>
2025-09-09 00:16:30 +0530

    Time: 3 ms (70.27%), Space: 16 MB (6.12%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,15 +3,34 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> ans; // vector to store ans
         int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans.push_back(i); // push i in vector
-                    ans.push_back(j); // push j in vector
-                    return ans;
-                }
-            }
+        unordered_map<int, int> m;
+        for (int i = 0; i < n; i++) {
+            int rem = target - nums[i];
+            if (m.find(rem) != m.end()) {
+                ans.push_back(m[rem]);
+                ans.push_back(i);
+            } else
+                m[nums[i]] = i;
         }
         return ans;
     }
 };
+
+// Brute force approach => T.C => O(n*n)
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         vector<int> ans; // vector to store ans
+//         int n = nums.size();
+//         for (int i = 0; i < n - 1; i++) {
+//             for (int j = i + 1; j < n; j++) {
+//                 if (nums[i] + nums[j] == target) {
+//                     ans.push_back(i); // push i in vector
+//                     ans.push_back(j); // push j in vector
+//                     return ans;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
