9c723461d1b7f850a576a8ca74115a9a2a4b4ba7
Aditya sharma <codebyaditya143@gmail.com>
2025-09-14 01:10:51 +0530

    Time: 0 ms (100%), Space: 13 MB (5.83%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int>dp;   //globally declared
+    int ftd(vector<int> arr, int i) {
+        if (i == arr.size() - 1) return arr[i];   // single house
+        if (i == arr.size() - 2) return max(arr[i], arr[i + 1]);   // 2 houses
+
+        if(dp[i] != -1) return dp[i];    // i th state already computed
+
+        //if dp[i] was -1 let's compute i th state for first time
+        return dp[i] = max(arr[i] + ftd(arr, i + 2), 0 + ftd(arr, i + 1));   
+    }
+    int rob(vector<int>& nums) {
+        dp.clear();
+        dp.resize(105,-1);
+        return ftd(nums, 0);
+    }
+};
+// recursive solution => Memory Limit Exceeded
+// class Solution {
+// public:
+//     int f(vector<int> arr, int i) {
+//         if (i == arr.size() - 1) return arr[i];   // single house
+//         if (i == arr.size() - 2) return max(arr[i], arr[i + 1]);   // 2 houses
+//         return max(arr[i] + f(arr, i + 2), 0 + f(arr, i + 1));
+//     }
+//     int rob(vector<int>& nums) { 
+//         return f(nums, 0);
+//     }
+// };
