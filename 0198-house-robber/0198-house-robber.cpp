8798ddbba1fc98c75874256064c34559c9885a92
Aditya sharma <codebyaditya143@gmail.com>
2025-09-14 13:02:45 +0530

    Time: 0 ms (100%), Space: 10.6 MB (46.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,45 @@
 class Solution {
 public:
     vector<int>dp;   //globally declared
-    int ftd(vector<int> arr, int i) {
-        if (i == arr.size() - 1) return arr[i];   // single house
-        if (i == arr.size() - 2) return max(arr[i], arr[i + 1]);   // 2 houses
+    int fbu(vector<int> arr) {
+        int n = arr.size();
+        if(n==1) return arr[0];
+        dp.clear();
+        dp.resize(n);
+        // base case
+        dp[n-1] = arr[n-1];
+        dp[n-2] = max(arr[n-1],arr[n-2]);
 
-        if(dp[i] != -1) return dp[i];    // i th state already computed
-
-        //if dp[i] was -1 let's compute i th state for first time
-        return dp[i] = max(arr[i] + ftd(arr, i + 2), 0 + ftd(arr, i + 1));   
+        for(int i = n-3;i>=0;i--){
+            dp[i] = max(arr[i]+dp[i+2],0+dp[i+1]);
+        }
+        return dp[0];
     }
     int rob(vector<int>& nums) {
-        dp.clear();
-        dp.resize(105,-1);
-        return ftd(nums, 0);
+        
+        return fbu(nums);
     }
 };
+
+// Top down dp
+// class Solution {
+// public:
+//     vector<int>dp;   //globally declared
+//     int ftd(vector<int> arr, int i) {
+//         if (i == arr.size() - 1) return arr[i];   // single house
+//         if (i == arr.size() - 2) return max(arr[i], arr[i + 1]);   // 2 houses
+
+//         if(dp[i] != -1) return dp[i];    // i th state already computed
+
+//         //if dp[i] was -1 let's compute i th state for first time
+//         return dp[i] = max(arr[i] + ftd(arr, i + 2), 0 + ftd(arr, i + 1));   
+//     }
+//     int rob(vector<int>& nums) {
+//         dp.clear();
+//         dp.resize(105,-1);
+//         return ftd(nums, 0);
+//     }
+// };
 // recursive solution => Memory Limit Exceeded
 // class Solution {
 // public:
