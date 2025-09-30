7bac41d743b2eab5e8ca97fd730566c5677f05e6
Aditya sharma <codebyaditya143@gmail.com>
2025-09-30 15:48:30 +0530

    Time: 0 ms (100%), Space: 71.8 MB (52.45%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum = 0;
+        int maxSum = INT_MIN;
+
+        for (int x : nums) {   // Travel all elements in nums
+            currSum += x;
+// Why this condition not here? => to tackle edge case where all elements are -ve
+            // if (currSum < 0)
+            //     currSum = 0;
+            maxSum = max(currSum, maxSum);
+            if (currSum < 0)
+                currSum = 0;
+        }
+        return maxSum;
+    }
+};
