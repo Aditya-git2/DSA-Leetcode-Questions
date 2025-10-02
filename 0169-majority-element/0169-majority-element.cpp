b6866c7387c766256b92ed5024f8d7825506f499
Aditya sharma <codebyaditya143@gmail.com>
2025-10-02 17:16:36 +0530

    Time: 2408 ms (5%), Space: 28.2 MB (63.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,12 +1,28 @@
 class Solution {
 public:
+
     int majorityElement(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int>freq;
-        for(int x:nums){
-            freq[x]++;
-            if(freq[x] > n/2) return x;
+        for (int val : nums) {
+            int freq = 0;
+            for (int ele : nums) {
+                if (ele == val)
+                    freq++;
+            }
+            if (freq > n / 2)
+                return val;
         }
-        return -1;   // if maj ele not found
+     // this line will never execute since majority element will always exist
+        return -1; 
     }
+
+    // int majorityElement(vector<int>& nums) {
+    //     int n = nums.size();
+    //     unordered_map<int,int>freq;
+    //     for(int x:nums){
+    //         freq[x]++;
+    //         if(freq[x] > n/2) return x;
+    //     }
+    //     return -1;   // if maj ele not found
+    // }
 };
