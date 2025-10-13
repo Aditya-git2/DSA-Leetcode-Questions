33403d761fc4a35867a5fa9b99808975dfd26f63
Aditya sharma <codebyaditya143@gmail.com>
2025-10-13 18:37:12 +0530

    Time: 99 ms (9.02%), Space: 14 MB (78.72%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,35 +1,34 @@
-class Solution {
+class Solution {    // Brute force approach => T.C => O(n*n)
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> ans; // vector to store ans
         int n = nums.size();
-        unordered_map<int, int> m;
-        for (int i = 0; i < n; i++) {
-            int rem = target - nums[i];
-            if (m.find(rem) != m.end()) {
-                ans.push_back(m[rem]);
-                ans.push_back(i);
-            } else
-                m[nums[i]] = i;
+        vector<int> ans; // vector to store answer
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans.push_back(i); // push i in ans
+                    ans.push_back(j); // push j in ans
+                    return ans;
+                }
+            }
         }
         return ans;
     }
 };
 
-// Brute force approach => T.C => O(n*n)
 // class Solution {
 // public:
 //     vector<int> twoSum(vector<int>& nums, int target) {
 //         vector<int> ans; // vector to store ans
 //         int n = nums.size();
-//         for (int i = 0; i < n - 1; i++) {
-//             for (int j = i + 1; j < n; j++) {
-//                 if (nums[i] + nums[j] == target) {
-//                     ans.push_back(i); // push i in vector
-//                     ans.push_back(j); // push j in vector
-//                     return ans;
-//                 }
-//             }
+//         unordered_map<int, int> m;
+//         for (int i = 0; i < n; i++) {
+//             int rem = target - nums[i];
+//             if (m.find(rem) != m.end()) {
+//                 ans.push_back(m[rem]);
+//                 ans.push_back(i);
+//             } else
+//                 m[nums[i]] = i;
 //         }
 //         return ans;
 //     }
