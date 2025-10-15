4450da4469498d973e02d68aec321deb37afac46
Aditya sharma <codebyaditya143@gmail.com>
2025-10-15 10:52:26 +0530

    Time: 3 ms (5.31%), Space: 12.5 MB (49%) - LeetHub

--- 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/1603-running-sum-of-1d-array.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
-    // Better approach
     vector<int> runningSum(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 1; i < n; i++) {
-            nums[i]+=nums[i-1];
-        }
-        return nums;
+        vector<int> result(nums.size());
+        partial_sum(nums.begin(), nums.end(), result.begin());
+        return result;
     }
 };
+
+// class Solution {
+// public:
+//     // Better approach
+//     vector<int> runningSum(vector<int>& nums) {
+//         int n = nums.size();
+//         for (int i = 1; i < n; i++) {
+//             nums[i]+=nums[i-1];
+//         }
+//         return nums;
+//     }
+// };
+
 // Method 1
 // class Solution {
 // public:
