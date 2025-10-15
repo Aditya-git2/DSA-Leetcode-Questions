c49ae5b0fe191adcec2d46e5d850d53b58165f24
Aditya sharma <codebyaditya143@gmail.com>
2025-10-15 10:38:25 +0530

    Time: 0 ms (100%), Space: 92.1 MB (18.52%) - LeetHub

--- 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/2212-removing-minimum-and-maximum-from-array.cpp
@@ -2,33 +2,55 @@ class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
         int n = nums.size();
-        int minVal = INT_MAX;
-        int maxVal = INT_MIN;
-        int minIdx = -1, maxIdx = -1;
+        int minIdx = 0, maxIdx = 0;
 
-        // Find indices of the minimum and maximum elements
-        for (int i = 0; i < n; i++) {
-            if (nums[i] < minVal) {
-                minVal = nums[i];
-                minIdx = i;
-            }
-            if (nums[i] > maxVal) {
-                maxVal = nums[i];
-                maxIdx = i;
-            }
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[minIdx]) minIdx = i;
+            if (nums[i] > nums[maxIdx]) maxIdx = i;
         }
 
-        // Option 1: Delete from front
-        int option1 = max(minIdx, maxIdx) + 1;
+        if (minIdx > maxIdx) swap(minIdx, maxIdx);
 
-        // Option 2: Delete from back
-        int option2 = n - min(minIdx, maxIdx);
+        int option1 = maxIdx + 1;               // front
+        int option2 = n - minIdx;               // back
+        int option3 = (minIdx + 1) + (n - maxIdx);  // both ends
 
-        // Option 3: Delete from both ends
-        int option3 = (minIdx + 1) + (n - maxIdx);
-        int option4 = (maxIdx + 1) + (n - minIdx);
-
-        // Return the minimum of all options
-        return min({option1, option2, option3, option4});
+        return min({option1, option2, option3});
     }
 };
+
+
+// class Solution {
+// public:
+//     int minimumDeletions(vector<int>& nums) {
+//         int n = nums.size();
+//         int minVal = INT_MAX;
+//         int maxVal = INT_MIN;
+//         int minIdx = -1, maxIdx = -1;
+
+//         // Find indices of the minimum and maximum elements
+//         for (int i = 0; i < n; i++) {
+//             if (nums[i] < minVal) {
+//                 minVal = nums[i];
+//                 minIdx = i;
+//             }
+//             if (nums[i] > maxVal) {
+//                 maxVal = nums[i];
+//                 maxIdx = i;
+//             }
+//         }
+
+//         // Option 1: Delete from front
+//         int option1 = max(minIdx, maxIdx) + 1;
+
+//         // Option 2: Delete from back
+//         int option2 = n - min(minIdx, maxIdx);
+
+//         // Option 3: Delete from both ends
+//         int option3 = (minIdx + 1) + (n - maxIdx);
+//         int option4 = (maxIdx + 1) + (n - minIdx);
+
+//         // Return the minimum of all options
+//         return min({option1, option2, option3, option4});
+//     }
+// };
