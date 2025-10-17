49f1b649a9a6be3c546bd86a8d441c585a1dc1cb
Aditya sharma <codebyaditya143@gmail.com>
2025-10-17 23:40:32 +0530

    Time: 3 ms (18.33%), Space: 26.8 MB (46.47%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -0,0 +1,106 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int n = height.size();
+        // prev greatest element array
+        vector<int> prev(n);
+        prev[0] = -1;
+        int max = height[0];
+        for (int i = 1; i < n; i++) {
+            prev[i] = max;
+            if (height[i] > max)
+                max = height[i];
+        }
+        max = height[n - 1];
+        // next greatest element array -> prev as next
+        prev[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (max < prev[i])
+                prev[i] = max;
+            if (max < height[i])
+                max = height[i];
+        }
+        // calculating water
+        int water = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (height[i] < prev[i]) {
+                water += (prev[i] - height[i]);
+            }
+        }
+        return water;
+    }
+};
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         // prev greatest element array
+//         vector<int> prev(n);
+//         prev[0] = -1;
+//         int max = height[0];
+//         for (int i = 1; i < n; i++) {
+//             prev[i] = max;
+//             if (height[i] > max)
+//                 max = height[i];
+//         }
+//         max = height[n - 1];
+//         // next greatest element array
+//         vector<int> next(n);
+//         next[n - 1] = -1;
+//         for (int i = n - 2; i >= 0; i--) {
+//             next[i] = max;
+//             if (max < height[i])
+//                 max = height[i];
+//         }
+//         for (int i = 0; i < n; i++) {
+//             prev[i] = min(prev[i], next[i]);
+//         }
+//         // calculating water
+//         int water = 0;
+//         for (int i = 1; i < n - 1; i++) {
+//             if (height[i] < prev[i]) {
+//                 water += (prev[i] - height[i]);
+//             }
+//         }
+//         return water;
+//     }
+// };
+
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int n = height.size();
+//         // prev greatest element array
+//         vector<int> prev(n);
+//         prev[0] = -1;
+//         int max = height[0];
+//         for (int i = 1; i < n; i++) {
+//             prev[i] = max;
+//             if (height[i] > max)
+//                 max = height[i];
+//         }
+//         max = height[n - 1];
+//         // next greatest element array
+//         vector<int> next(n);
+//         next[n - 1] = -1;
+//         for (int i = n - 2; i >= 0; i--) {
+//             next[i] = max;
+//             if (max < height[i])
+//                 max = height[i];
+//         }
+//         // minimum array
+//         vector<int> mini(n);
+//         for (int i = 0; i < n; i++) {
+//             mini[i] = min(prev[i], next[i]);
+//         }
+//         // calculating water
+//         int water = 0;
+//         for (int i = 1; i < n - 1; i++) {
+//             if (height[i] < mini[i]) {
+//                 water += (mini[i] - height[i]);
+//             }
+//         }
+//         return water;
+//     }
+// };
