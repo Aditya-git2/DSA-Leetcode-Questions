c52e795f043a694e8375b9ddbe3875035f1a5277
Aditya sharma <codebyaditya143@gmail.com>
2025-11-08 15:38:49 +0530

    Time: 0 ms (100%), Space: 11.1 MB (75.3%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.cpp
@@ -1,35 +1,41 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> sorted = nums;
-        sort(sorted.begin(), sorted.end());
+        int count = 0;
         int n = nums.size();
-        
-        // Try all rotations of sorted array
-        for (int i = 0; i < n; ++i) {
-            bool match = true;
-            for (int j = 0; j < n; ++j) {
-                if (nums[j] != sorted[(i + j) % n]) {
-                    match = false;
-                    break;
-                }
+        for (int i = 0; i < n; i++) {
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
-            if (match) return true;
         }
-        return false;
+        // if count is less than equal to 1 return true,else false
+        return count <= 1; 
     }
 };
 
 // class Solution {
 // public:
+//     // T.C => O(n*n)   S.C => O(n)   {brute force approach}
+//     // idea ;- sort array => try every rotation of a sorted array=> if found
+//     any
+//     // match return true
 //     bool check(vector<int>& nums) {
-//         int count = 0;
+//         vector<int> sorted = nums;
+//         sort(sorted.begin(), sorted.end());
 //         int n = nums.size();
+
+//         // Try all rotations of sorted array
 //         for (int i = 0; i < n; ++i) {
-//             if (nums[i] > nums[(i + 1) % n]) {
-//                 count++;
+//             bool match = true;
+//             for (int j = 0; j < n; ++j) {
+//                 if (nums[j] != sorted[(i + j) % n]) {
+//                     match = false;
+//                     break;
+//                 }
 //             }
+//             if (match)
+//                 return true;
 //         }
-//         return count <= 1;
+//         return false;
 //     }
 // };
